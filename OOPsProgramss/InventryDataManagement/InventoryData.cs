4d1744e3fa409efe2b08afd9f1e28b332757d8e0
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-29 13:13:41 +0530

    [Sunny] Add : Inventory_Food_Data

---
+++ OOPsProgramss/InventryDataManagement/InventoryData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsProgramss.InventryDataManagement
+{
+    public class InventoryData
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int PricePerKg { get; set; }
+    }
+}
---
+++ OOPsProgramss/Program.cs
@@ -0,0 +1,27 @@
+﻿using OOPsProgramss.InventryDataManagement;
+using System;
+namespace OOPsProgramss
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Welcome To The OOPs Programs *****");
+            const string Inventory_File_path = @"E:\BridgeLabzProject\OOPsPrograms\OOPsProgram\OOPsProgramss\InventryDataManagement\Inventory.json";
+            InventoryManagement Data = new InventoryManagement();
+            Console.WriteLine("1. Display \n");
+            bool check = true;
+            while (check)
+            {
+                //Console.WriteLine("Enter The Input That Option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Data.DisplayInventory(Inventory_File_path);
+                        break;
+                }
+            }
+        }
+    }
+}
