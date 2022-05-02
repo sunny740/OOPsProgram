4f029f56c5dbc3c336266b4038d28b6a7d3a12de
sunny740 <sunnysejwarvitm005@gmail.com>
2022-05-03 00:31:10 +0530

    [Sunny] Add : Stock Account Management is Added

---
+++ OOPsProgramss/InventoryStockAccountMgmt/StockData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsProgramss.InventoryStockAccountMgmt
+{
+    public class StockData
+    {
+        public string ShareName { get; set; }
+        public int NumberofShare { get; set; }
+        public int SharePrice { get; set; }
+    }
+}
---
+++ OOPsProgramss/InventoryStockAccountMgmt/StockManagement.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsProgramss.InventoryStockAccountMgmt
+{
+    public class StockManagement
+    {
+        public void DisplayInventory(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                var json = reader.ReadToEnd();
+                var inventory = JsonConvert.DeserializeObject<List<StockData>>(json);
+                Console.WriteLine("ShareName" + "\t" + "NumberofShare " + "\t" + "SharePrice" + "\t" + "TotalShareValue");
+                foreach (var data in inventory)
+                {
+                    Console.WriteLine(data.ShareName + "\t\t " + data.NumberofShare + "\t\t" + data.SharePrice + "\t\t" + (data.NumberofShare * data.SharePrice));
+                }
+            }
+        }
+    }
+}
